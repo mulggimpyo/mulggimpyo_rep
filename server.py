fc081125077a0847ec646e8aeb4f74c4c6707827
mulggimpyo <mulggimpyo@gmail.com>
2019-09-13 13:29:23 +0900

    test

--- server.py
+++ server.py
@@ -25,23 +25,9 @@ saver.restore(sess, save_path)
 @app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        return render_template('index.html')
+        return render_template('test.html')
     if request.method == 'POST':
-        avg_temp = float(request.form['avg_temp'])
-        min_temp = float(request.form['min_temp'])
-        max_temp = float(request.form['max_temp'])
-        rain_fall = float(request.form['rain_fall'])
-
-    price = 0
-    data = ((avg_temp, min_temp, max_temp, rain_fall),)
-    arr = np.array(data, dtype=np.float32)
-
-    x_data = arr[0:4]
-    dict = sess.run(hypothesis, feed_dict={X: x_data})
-    print(dict[0])
-
-    price = dict[0]
-    return render_template('index.html', price=price)
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
